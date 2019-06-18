6c4c7874d3bc9c2bf1d7db8bdd9d8ce2a1ae6d1f
Kiran Anto <kirananto@gmail.com>
2019-06-18 20:15:12 +0530

    Guard the code from SSR in Vuepress as it uses browser APIs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,26 @@
-export default ({ Vue }) => {
-    Vue.mixin({
-      computed: {
-        esper_tenant_name() {
-          const esper_tenant_name = document.cookie.replace(/(?:(?:^|.*;\s*)esper_tenant_name\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-          return esper_tenant_name === '' ? 'foo' : esper_tenant_name
-        },
+export default ({ Vue }) =>
+  Vue.mixin({
+    data: function() {
+      return {
+        esper_tenant: "foo"
+      };
+    },
+    computed: {
+      esper_tenant_name() {
+        return this.esper_tenant;
       }
-    })
-  }
+    },
+    mounted() {
+      if (document) {
+        const esper_tenant_temp =
+          document &&
+          document.cookie &&
+          document.cookie.replace(
+            /(?:(?:^|.*;\s*)esper_tenant_name\s*\=\s*([^;]*).*$)|^.*$/,
+            "$1"
+          );
+        this.esper_tenant =
+          esper_tenant_temp === "" ? "foo" : esper_tenant_temp;
+      }
+    }
+  });
