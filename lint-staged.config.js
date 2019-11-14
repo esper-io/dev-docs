c1f3867171b92be4ca12c44f28e228253f4d3e1a
Kiran Anto <kirananto@gmail.com>
2019-11-14 11:53:54 +0530

    Check husky issues only for md and assets

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '*.json': ['prettier --write', 'git add'],
-  '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write', 'git add'],
   '*.scss': ['stylelint --fix', 'prettier --write', 'git add'],
   '*.md': ['markdownlint */*.md docs/*/*.md docs/home/*/*.md docs/*/*/*/*.md docs/*/*/*/*/*.md docs/*/*/*/*/*/*.md  docs/*/*/*/*/*/*/*.md'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
