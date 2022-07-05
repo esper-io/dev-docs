77d568dc49d7619ecd4e68284937c1762be8b094
Radha Kiwalkar <radha@esper.io>
2022-07-05 12:47:52 -0700

    trying FAQ menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -257,51 +257,51 @@ module.exports = {
         },
         // "tutorials",
         // "support",
-        // "faq"
-        {
-          title: 'Frequently Asked Questions',
-          collapsable: true,
-          children: [
-            {
-              title: 'Technical',
-              //path: "faq/technical/README.md",
-              children: [
-               {
-                 title: 'SUNFYRE_V7',
-                 path: "release-notes/sdk/rel-3.0"
-               },
-              //   // {
-              //   //   title: 'SUNFYRE_V6',
-              //   //   path: "release-notes/sdk/rel-2.0"
-              //   // },
-              //   // {
-              //   //   title: 'SUNFYRE_V5',
-              //   //   path: "release-notes/sdk/rel-1.0"
-              //   // },
-              ],
-            },
-            {
-              title: 'Non-technical',
-              //path: "faq/non-technical/README.md",
-               children: [
-               {
-                 title: 'v6.2.0',
-                 path: "release-notes/provisioner/pro-rel-1.0"
-               },
-              //   // {
-              //   //   title: 'SUNFYRE_V6',
-              //   //   path: "release-notes/sdk/rel-2.0"
-              //   // },
-              //   // {
-              //   //   title: 'SUNFYRE_V5',
-              //   //   path: "release-notes/sdk/rel-1.0"
-              //   // },
-              ],
-            },
+         "faq/"
+        // {
+        //   title: 'Frequently Asked Questions',
+        //   collapsable: true,
+        //   children: [
+        //     {
+        //       title: 'Technical',
+        //       //path: "faq/technical/README.md",
+        //       children: [
+        //        {
+        //          title: 'SUNFYRE_V7',
+        //          path: "release-notes/sdk/rel-3.0"
+        //        },
+        //       //   // {
+        //       //   //   title: 'SUNFYRE_V6',
+        //       //   //   path: "release-notes/sdk/rel-2.0"
+        //       //   // },
+        //       //   // {
+        //       //   //   title: 'SUNFYRE_V5',
+        //       //   //   path: "release-notes/sdk/rel-1.0"
+        //       //   // },
+        //       ],
+        //     },
+        //     {
+        //       title: 'Non-technical',
+        //       //path: "faq/non-technical/README.md",
+        //        children: [
+        //        {
+        //          title: 'v6.2.0',
+        //          path: "release-notes/provisioner/pro-rel-1.0"
+        //        },
+        //       //   // {
+        //       //   //   title: 'SUNFYRE_V6',
+        //       //   //   path: "release-notes/sdk/rel-2.0"
+        //       //   // },
+        //       //   // {
+        //       //   //   title: 'SUNFYRE_V5',
+        //       //   //   path: "release-notes/sdk/rel-1.0"
+        //       //   // },
+        //       ],
+        //     },
             
             
-          ]
-        }
+        //   ]
+        // }
 
       ],
     },
